58efc052b43d08642fb62450254589e40b4b078d
Yassen Bantchev <yassenb@gmail.com>
2013-05-20 16:25:07 +0300

    A solution to http://rosettacode.org/wiki/I_before_E_except_after_C

---
+++ i-before-e/i-before-e.hs
@@ -0,0 +1,22 @@
+import Control.Applicative ((<$>))
+
+-- requires the regex-posix and text-format-simple packages
+import Text.Regex.Posix ((=~))
+import Text.Format (format)
+
+plausibleStr :: String -> String -> [String] -> String
+plausibleStr regex1 regex2 words = format "{0} with a ratio of {1} ({2} / {3})"
+    [description, show ratio, show nPositive, show nNegative] where
+        description = if plausible then "plausible" else "implausible" where
+            plausible = ratio > 2
+        nPositive = nOccurences regex1
+        nNegative = nOccurences regex2
+        ratio = fromIntegral nPositive / fromIntegral nNegative
+        nOccurences regex = length $ filter (=~ regex) words where
+
+main = do
+    words <- lines <$> getContents
+    putStrLn $ format
+        ("I before E when not preseded by C is {0}\n" ++
+        "E before I when preseded by C is {1}")
+        [plausibleStr "[^c]ie" "[^c]ei" words, plausibleStr "cei" "cie" words]
